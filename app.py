45de0f9cae9fe098d8a7edabb4d23f8b5747cf20
Baron Ningthoujam <baronningthoujam109@gmail.com>
2026-06-03 21:23:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load model
+model = tf.keras.models.load_model("my-model.keras")
+
+st.title("♻️ Waste Classification App")
+
+st.write("Upload an image of waste and the model will predict the category.")
+
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Preprocess image
+    img = image.resize((224, 224))  # change if your model uses different size
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Prediction
+    prediction = model.predict(img_array)
+
+    st.write("### Prediction Output:")
+    st.write(prediction)

1a0a7fb5b870286c4721f39a196da9aea32a5cf7
Baron Ningthoujam <baronningthoujam109@gmail.com>
2026-06-03 23:18:07 +0530

    Fix input image size

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Preprocess image
-    img = image.resize((224, 224))  # change if your model uses different size
+    img = image.resize((244, 244)) # change if your model uses different size
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
